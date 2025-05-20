26ab0aa506a7da29b9615ea3a904c98c0217c9ea
evanschmelkin <evanschmelkin@gmail.com>
2025-05-20 15:16:01 -0400

    for the cloud

--- db/migrate/20250520185409_drop_microposts_table.rb
+++
@@ -1,9 +0,0 @@
-class DropMicropostsTable < ActiveRecord::Migration[7.0]
-  def up
-    drop_table :microposts
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_05_20_190015) do
-  create_table "books", force: :cascade do |t|
-    t.string "title"
-    t.string "author"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+ActiveRecord::Schema[7.2].define(version: 2025_05_20_191404) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.text "description"
