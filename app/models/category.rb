8dcbb1fd58ab9a221b47deba60e7e6239c54003f
evanschmelkin <evanschmelkin@gmail.com>
2025-05-20 14:43:56 -0400

    working from my holiday in tenerife

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ApplicationRecord
   belongs_to :user
   has_many :todos, dependent: :destroy
+
+  validates :name, presence: true #fix from 5/20
+  validates :description, presence: true #fix from 5/20
 end
